e0a86c7a9076734822e130133d234de424cb6c9d
Slimikus <slimikus@gmail.com>
2016-02-01 16:26:32 +0300

    + добавлено уведомление напоминания

--- My ToDoList/My ToDoList/DetailViewController.m
+++ My ToDoList/My ToDoList/DetailViewController.m
@@ -58,6 +58,25 @@
 
 - (void) save {
     
+    NSString * eventInfo = self.textField.text;
+    NSDateFormatter * formater = [[NSDateFormatter alloc] init];
+    formater.dateFormat = @"HH:mm dd.MMMM.yyyy";
+    
+    NSString * eventDate = [formater stringFromDate:self.eventDate];
+    
+    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:eventInfo, @"eventInfo", eventDate, @"eventDate", nil];
+    
+    UILocalNotification * notification = [[UILocalNotification alloc] init];
+    notification.userInfo = dict;
+    notification.timeZone = [NSTimeZone defaultTimeZone];
+    notification.fireDate = self.eventDate;
+    notification.alertBody = eventInfo;
+    notification.applicationIconBadgeNumber = 1;
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    
+    
     NSLog(@"save");
     
 }

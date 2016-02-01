f4aac5f6b6b665d45f6e84c5f11b2d9ebe1b0120
Slimikus <slimikus@gmail.com>
2016-02-01 13:55:39 +0300

    Создание проекта Добавление кнопки "+"

---
+++ My ToDoList/My ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My ToDoList
+//
+//  Created by Admin on 01.02.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My ToDoList/My ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My ToDoList
+//
+//  Created by Admin on 01.02.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

3060b812bd5b55780c628cbb55bd4fbe5662575d
Slimikus <slimikus@gmail.com>
2016-02-04 10:15:39 +0300

    создание табличного вида

---
+++ My ToDoList/My ToDoList/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  My ToDoList
+//
+//  Created by Admin on 04.02.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
